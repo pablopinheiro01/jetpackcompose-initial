52c0f4c13d42c96df0a25e0c8ac1c27310f271ef
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2023-02-02 18:06:06 -0300

    Alterado para corrigir layout na tela e realizado desafio

--- app/src/main/java/br/com/pablo/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/pablo/aluvery/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -21,9 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import br.com.pablo.aluvery.ui.theme.AluveryTheme
-import br.com.pablo.aluvery.ui.theme.Purple500
-import br.com.pablo.aluvery.ui.theme.Teal200
+import br.com.pablo.aluvery.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +33,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    ProductItem()
+//                    ProductItem()
+                    DesafioNumberTwo()
                 }
             }
         }
@@ -42,40 +45,97 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column(
-        Modifier
-            .height(250.dp)
-            .width(200.dp)
+    Surface(
+        shape = RoundedCornerShape(15.dp),
+        elevation = 4.dp
     ) {
-        Box(
+        Column(
             Modifier
-                .height(100.dp)
-                .background(
-                    brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)),
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                Modifier
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)),
+                    )
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "teste",
+                    modifier = Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize / 2)
+                        .clip(shape = CircleShape)
+                        .align(BottomCenter)
                 )
-                .fillMaxWidth()
-        ){
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = "teste",
-                modifier = Modifier
-                    .size(100.dp)
-                    .offset(y = 50.dp)
-                    .clip(shape = CircleShape)
-                    .align(BottomCenter)
-            )
-        }
-        Spacer(modifier = Modifier.height(50.dp))
-        Column(Modifier.padding(16.dp)) {
-            Text(
-                text = LoremIpsum(50).values.first(),
-                fontSize = 18.sp,
-                fontWeight = FontWeight(700),
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(modifier = Modifier.padding(top = 8.dp), text = "R$ 14,99", fontSize = 14.sp, fontWeight = FontWeight(400))
+            }
+            Spacer(modifier = Modifier.height(imageSize / 2))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    modifier = Modifier.padding(top = 8.dp),
+                    text = "R$ 14,99",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
         }
     }
+}
 
+@Preview
+@Composable
+fun DesafioNumberTwo() {
+    Surface {
+        Row(
+            Modifier
+                .widthIn(250.dp, 400.dp)
+                .height(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                Modifier
+                    .width(imageSize)
+                    .background(
+                        brush = Brush.verticalGradient(colors = listOf(Purple200, Teal200)),
+                    )
+                    .fillMaxHeight()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "teste",
+                    modifier = Modifier
+                        .size(imageSize)
+                        .offset(x = imageSize / 2)
+                        .border
+                            (
+                            width = 2.dp,
+                            brush = Brush.horizontalGradient(colors = listOf(Purple700, Teal200)),
+                            shape = CircleShape
+                        )
+                        .clip(shape = CircleShape)
+                        .align(Center)
+                )
+            }
+            Spacer(modifier = Modifier.width(imageSize/2))
+            Text(
+                text = LoremIpsum(words = 50).values.first(),
+                modifier = Modifier.padding(18.dp),
+                fontSize = 18.sp,
+                fontWeight = FontWeight(700),
+                maxLines = 6,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
 }
--- app/src/main/java/br/com/pablo/aluvery/ui/components/TestComponents.kt
+++ app/src/main/java/br/com/pablo/aluvery/ui/components/TestComponents.kt
@@ -1,15 +1,77 @@
 package br.com.pablo.aluvery.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.pablo.aluvery.ui.theme.Purple200
+import br.com.pablo.aluvery.ui.theme.Purple700
+import br.com.pablo.aluvery.ui.theme.Teal200
 
 
+//comentado para nao ficar quebrando o cod
+//@Preview
+//@Composable
+//fun DesafioNumberTwo() {
+//    Surface {
+//        Row(
+//            Modifier
+//                .widthIn(250.dp, 400.dp)
+//                .height(200.dp)
+//        ) {
+//            val imageSize = 100.dp
+//            Box(
+//                Modifier
+//                    .width(imageSize)
+//                    .background(
+//                        brush = Brush.verticalGradient(colors = listOf(Purple200, Teal200)),
+//                    )
+//                    .fillMaxHeight()
+//            ) {
+//                Image(
+//                    painter = painterResource(id = R.drawable.ic_launcher_background),
+//                    contentDescription = "teste",
+//                    modifier = Modifier
+//                        .size(imageSize)
+//                        .offset(x = imageSize / 2)
+//                        .border
+//                            (
+//                            width = 2.dp,
+//                            brush = Brush.horizontalGradient(colors = listOf(Purple700, Teal200)),
+//                            shape = CircleShape
+//                        )
+//                        .clip(shape = CircleShape)
+//                        .align(Alignment.Center)
+//                )
+//            }
+//            Spacer(modifier = Modifier.width(imageSize/2))
+//            Text(
+//                text = LoremIpsum(words = 50).values.first(),
+//                modifier = Modifier.padding(18.dp),
+//                fontSize = 18.sp,
+//                fontWeight = FontWeight(700),
+//                maxLines = 6,
+//                overflow = TextOverflow.Ellipsis
+//            )
+//        }
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
